fd4e31cbaddf2a4eaef18621e2d9ca50ebd1c18c
Johannes Häggqvist <johanneshagg@gmail.com>
2010-12-17 15:52:07 +0000

    - Added license stuff

--- include/ProtoZed/AppState.h
+++ include/ProtoZed/AppState.h
@@ -1,3 +1,21 @@
+/*
+	Copyright 2010 Johannes Häggqvist
+
+	This file is part of ProtoZed.
+
+	ProtoZed is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	ProtoZed is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with ProtoZed.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #ifndef AppState_h__
 #define AppState_h__
 
--- include/ProtoZed/Singleton.h
+++ include/ProtoZed/Singleton.h
@@ -1,3 +1,21 @@
+/*
+	Copyright 2010 Johannes Häggqvist
+
+	This file is part of ProtoZed.
+
+	ProtoZed is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	ProtoZed is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with ProtoZed.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #ifndef Singleton_h__
 #define Singleton_h__
 
@@ -12,8 +30,6 @@ namespace PZ
 		Singleton()
 		{
 			assert(!ms_singleton);
-			//use a cunning trick to get the singleton pointing to the start of the whole, rather than
-			//the start of the Singleton part of the object
 			int offset = (int)(T*)1 - (int)(Singleton <T>*)(T*)1;
 			ms_singleton = (T*)((int)this + offset);
 		}
